6d04891868114b63848c98d8c4eccba5cbc0c17e
Francisco Axel Roman Cardoza <franaxel@live.com.mx>
2024-05-16 15:22:22 -0600

    Se agrego el sensor de gestos y se agrego la funcionalidad para cambiar canales por medio de gestos

--- ControlTV/ControlTV.ino
+++ ControlTV/ControlTV.ino
@@ -1,7 +1,18 @@
 #include <IRremote.h>
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+// Pins
+#define APDS9960_INT    2 // Needs to be an interrupt pin
+
+// Constants
+
+// Global Variables
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+int isr_flag = 0;
 
 #define IR_LED_PIN 3
-const int pinIRSensor = 2;
+const int pinIRSensor = 4;
 const int ledPin = 13; // Define el pin al que está conectado el LED IR
 const int trigPin = 9; // Pin del trig para el sensor ultrasónico
 const int echoPin = 10; // Pin del echo para el sensor ultrasónico
@@ -15,19 +26,79 @@ unsigned long channelUpCode = 0x2FDD827; // Código IR para subir canal
 unsigned long channelDownCode = 0x2FDF807; // Código IR para bajar canal
 unsigned long muteCode = 0x2FD08F7; // Código IR para silenciar
 
+void interruptRoutine() {
+  isr_flag = 1;
+}
+
 void setup() {
   Serial.begin(115200); // Inicializar el puerto serie
   pinMode(ledPin, OUTPUT); // Configura el pin como salida
   pinMode(trigPin, OUTPUT); // Configura el pin del trig como salida
   pinMode(echoPin, INPUT); // Configura el pin del echo como entrada
+
+  // Set interrupt pin as input
+  pinMode(APDS9960_INT, INPUT);
+  // Initialize interrupt service routine
+  attachInterrupt(0, interruptRoutine, FALLING);
+
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Start running the APDS-9960 gesture sensor engine
+  if ( apds.enableGestureSensor(true) ) {
+    Serial.println(F("Gesture sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during gesture sensor init!"));
+  }
+}
+
+
+void handleGesture() {
+    if ( apds.isGestureAvailable() ) {
+    switch ( apds.readGesture() ) {
+      case DIR_UP:
+        Serial.println("UP");
+        break;
+      case DIR_DOWN:
+        Serial.println("DOWN");
+        break;
+      case DIR_LEFT:
+      irsend.sendNEC(channelDownCode, 32);
+        Serial.println("LEFT");
+        break;
+      case DIR_RIGHT:
+      irsend.sendNEC(channelUpCode, 32);
+        Serial.println("RIGHT");
+        break;
+      case DIR_NEAR:
+        Serial.println("NEAR");
+        break;
+      case DIR_FAR:
+        Serial.println("FAR");
+        break;
+      default:
+        Serial.println("NONE");
+    }
+  }
 }
 
 bool encendido = false;
 void loop() {
+  if( isr_flag == 1 ) {
+    detachInterrupt(0);
+    handleGesture();
+    isr_flag = 0;
+    attachInterrupt(0, interruptRoutine, FALLING);
+  }
+
   
   int sensorValue = digitalRead(pinIRSensor);
   if(sensorValue == HIGH && !encendido){
-    irsend.sendNEC(channelUpCode, 32);
+    irsend.sendNEC(powerCode, 32);
 
     digitalWrite(ledPin, HIGH); // Enciende el LED IR
     delay(1000); // Espera 1 segundo
@@ -59,12 +130,12 @@ void loop() {
   // Si la distancia es menor o igual a 10 cm, baja el volumen
   if (distance <= 10 && distance >= 5) {
     irsend.sendNEC(volumeDownCode, 32);
-    delay(50);
+    //delay(50);
   }
   // Si la distancia es mayor a 15 pero menor a 20 cm, sube el volumen
   else if (distance > 15 && distance < 20) {
     irsend.sendNEC(volumeUpCode, 32);
-    delay(50);
+    //delay(50);
   }
 
   // Escuchar el puerto serie para comandos desde la computadora
