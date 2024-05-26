af0b29f829de781d37c02757fbb001cde6a7089f
Francisco Axel Roman Cardoza <franaxel@live.com.mx>
2024-05-26 15:15:45 -0600

    Codigo final y codigos de envio de los controles para megacable, LG y Spectra

--- ControlTV/ControlTV.ino
+++ ControlTV/ControlTV.ino
@@ -42,6 +42,7 @@ const int echoPin = 10; // Pin del echo para el sensor ultrasónico
 IRsend irsend(IR_LED_PIN);
 
 //Spectra
+/*
 unsigned long powerCode = 0x2FD48B7; // Código IR para encender/apagar
 unsigned long volumeUpCode = 0x2FD58A7; // Código IR para subir volumen
 unsigned long volumeDownCode = 0x2FD7887; // Código IR para bajar volumen
@@ -66,6 +67,55 @@ unsigned long menuCode = 0x2FDDA25    ;
 
 unsigned long netflixCode = 0xBB44BF40; // Código IR para Netflix
 unsigned long disneyCode = 0x3A00 ; // Código IR para Disney+
+*/
+
+
+/*--------------------   MEGACABLE   -----------------------------*/
+/*
+unsigned long powerCode = 0xCD323BC4; // Código IR para encender/apagar
+unsigned long volumeUpCode = 0xCD3201FE; // Código IR para subir volumen
+unsigned long volumeDownCode = 0xCD32817E; // Código IR para bajar volumen
+unsigned long channelUpCode = 0xCD32A15E; // Código IR para subir canal
+unsigned long channelDownCode = 0xCD32619E; // Código IR para bajar canal
+unsigned long muteCode = 0xCD3239C6; // Código IR para silenciar
+
+unsigned long sourceCode = 0xE0E0807F; // Código IR para home
+unsigned long upCode =0xCD3253AC ; // Código IR para arriba
+unsigned long downCode = 0xCD324BB4  ; // Código IR para abajo
+unsigned long leftCode = 0xCD329966  ; // Código IR para izquierda
+unsigned long rightCode = 0xCD32837C  ; // Código IR para derecha
+unsigned long okCode = 0xCD32738C  ; // Código IR para ok
+unsigned long backCode = 0x2FDFF00 ; // Código IR para regresar
+
+unsigned long playPauseCode = 0xCD32A956 ; // Código IR para play/pause
+
+unsigned long exitCode = 0xCD32A35C  ;
+
+unsigned long menuCode = 0xCD3211EE    ; // Código IR para Netflix
+unsigned long disneyCode = 0x3A00 ; // Código IR para Disney+
+*/
+
+/*-------------------------   LG   -----------------------------*/
+unsigned long powerCode = 0x20DF10EF;       // Código IR para encender/apagar
+unsigned long volumeUpCode = 0x20DF40BF;    // Código IR para subir volumen
+unsigned long volumeDownCode = 0x20DFC03F ;  // Código IR para bajar volumen
+unsigned long channelUpCode = 0x20DF00FF;   // Código IR para subir canal
+unsigned long channelDownCode = 0x20DF807F; // Código IR para bajar canal
+unsigned long muteCode = 0x20DF906F;        // Código IR para silenciar
+unsigned long sourceCode = 0x20DF3EC1;      // Código IR para home
+unsigned long upCode = 0x20DF02FD;          // Código IR para arriba
+unsigned long downCode = 0x20DF827D;        // Código IR para abajo
+unsigned long leftCode = 0x20DFE01F;        // Código IR para izquierda
+unsigned long rightCode = 0x20DF609F;       // Código IR para derecha
+unsigned long okCode = 0x20DF22DD;          // Código IR para ok
+unsigned long backCode = 0x20DF14EB;        // Código IR para regresar
+unsigned long playPauseCode = 0x20DF0DF2;   // Código IR para play/pause
+unsigned long exitCode = 0x20DFDA25;        // Código IR para salir
+unsigned long menuCode = 0x20DFC23D;        // Código IR para menú
+unsigned long netflixCode = 0x20DF6A95;     // Código IR para Netflix
+unsigned long disneyCode = 0x20DF3AC5;      // Código IR para Disney+
+
+
 
 void interruptRoutine() {
   isr_flag = 1;
@@ -73,7 +123,6 @@ void interruptRoutine() {
 
 void setup() {
   Serial.begin(115200); // Inicializar el puerto serie
-//  pinMode(ledPin, OUTPUT); // Configura el pin como salida
   pinMode(trigPin, OUTPUT); // Configura el pin del trig como salida
   pinMode(echoPin, INPUT); // Configura el pin del echo como entrada
   pinMode(LED_PIN,OUTPUT);
@@ -96,8 +145,6 @@ void setup() {
 
   pinMode(mute, INPUT_PULLUP); // Configura el pin play con resistencia pull-up
 
-  //pinMode(exit, INPUT_PULLUP); // Configura el pin up con resistencia pull-up
- // pinMode(down, INPUT_PULLUP); // Configura el pin down con resistencia pull-up
 }
 
 void encender(){
@@ -109,51 +156,41 @@ void encender(){
 bool encendido = false;
 bool channel = false;
 void loop() {
-
   int sensorPower = digitalRead(power);
   if(sensorPower == HIGH && !encendido){
     irsend.sendNEC(powerCode, 32);
-    //delay(50);
     encendido = true;
     encender();
   }
-
-
   int sensorChannelUp = digitalRead(channelUp);
   if(sensorChannelUp == LOW){
     irsend.sendNEC(channelUpCode, 32);
     delay(50);
     encender();
   }
-
   int sensorChannelDown = digitalRead(channelDown);
   if(sensorChannelDown == LOW){
     irsend.sendNEC(channelDownCode, 32);
     delay(50);
     encender();
   }
-
   int sensorSource = digitalRead(source);
   if(sensorSource == LOW){
     irsend.sendNEC(sourceCode, 32);
     delay(50);
     encender();
   }
-  
   int sensorOk = digitalRead(ok);
   if(sensorOk == LOW){
     irsend.sendNEC(okCode, 32);
     delay(50);
     encender();
   }
-
-
   int sensorUp = digitalRead(up);
   if(sensorUp == LOW){
     irsend.sendNEC(upCode, 32);
     delay(50);
   }
-
   int sensorDown = digitalRead(down);
   if(sensorDown == LOW){
     irsend.sendNEC(downCode, 32);
@@ -166,7 +203,6 @@ void loop() {
     delay(50);
     encender();
   }
-
   int sensorleft = digitalRead(left);
   if(sensorleft == LOW){
     irsend.sendNEC(leftCode, 32);
@@ -180,7 +216,6 @@ void loop() {
     delay(50);
     encender();
   }
-
   int sensorMute = digitalRead(mute);
   if(sensorMute == LOW){
     irsend.sendNEC(muteCode, 32);
@@ -195,29 +230,22 @@ void loop() {
     encender();
   }
 */
-
   int sensorMenu = digitalRead(menu);
   if(sensorMenu == LOW){
     irsend.sendNEC(menuCode, 32);
     delay(50);
     encender();
   }
-
   int sensorExit = digitalRead(exitPort);
   if(sensorExit == LOW){
     irsend.sendNEC(exitCode, 32);
     delay(50);
     encender();
   }
-
   // Si el sensor vuelve a LOW, se restablece la variable encendido
   if(sensorPower == LOW){
     encendido = false;
   }
-  
-
-
-
   // Lee la distancia del sensor ultrasónico
   long duration, distance;
   digitalWrite(trigPin, LOW);  
@@ -232,13 +260,11 @@ void loop() {
   if (distance <= 13 && distance >= 2) {
     irsend.sendNEC(volumeDownCode, 32);
     digitalWrite(LED_PIN,HIGH);
-    //delay(10);
   }
   // Si la distancia es mayor a 15 pero menor a 20 cm, sube el volumen
   else if (distance > 18 && distance < 35) {
     irsend.sendNEC(volumeUpCode, 32);
     digitalWrite(LED_PIN,HIGH);
-    //delay(10);
   }
   else{
   digitalWrite(LED_PIN,LOW);
--- ReceiveDump/ReceiveDump.ino
+++ ReceiveDump/ReceiveDump.ino
@@ -1,199 +1,38 @@
-/*
- * ReceiveDump.cpp
- *
- * Dumps the received signal in different flavors.
- * Since the printing takes so much time (200 ms @115200 for NEC protocol, 70ms for NEC repeat),
- * repeat signals may be skipped or interpreted as UNKNOWN.
- *
- *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
- *
- ************************************************************************************
- * MIT License
- *
- * Copyright (c) 2020-2024 Armin Joachimsmeyer
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is fur  nished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- ************************************************************************************
- */
 #include <Arduino.h>
-#include <IRremote.h>
-
-#include "PinDefinitionsAndMore.h" // Define macros for input and output pin etc.
-
-#if !defined(RAW_BUFFER_LENGTH)
-#  if RAMEND <= 0x4FF || RAMSIZE < 0x4FF
-#define RAW_BUFFER_LENGTH  180  // 750 (600 if we have only 2k RAM) is the value for air condition remotes. Default is 112 if DECODE_MAGIQUEST is enabled, otherwise 100.
-#  elif RAMEND <= 0x8FF || RAMSIZE < 0x8FF
-#define RAW_BUFFER_LENGTH  600  // 750 (600 if we have only 2k RAM) is the value for air condition remotes. Default is 112 if DECODE_MAGIQUEST is enabled, otherwise 100.
-#  else
-#define RAW_BUFFER_LENGTH  750  // 750 (600 if we have only 2k RAM) is the value for air condition remotes. Default is 112 if DECODE_MAGIQUEST is enabled, otherwise 100.
-#  endif
-#endif
-
-/*
- * MARK_EXCESS_MICROS is subtracted from all marks and added to all spaces before decoding,
- * to compensate for the signal forming of different IR receiver modules. See also IRremote.hpp line 142.
- *
- * You can change this value accordingly to the receiver module you use.
- * The required value can be derived from the timings printed here.
- * Keep in mind that the timings may change with the distance
- * between sender and receiver as well as with the ambient light intensity.
- */
-#define MARK_EXCESS_MICROS    20    // Adapt it to your IR receiver module. 20 is recommended for the cheap VS1838 modules.
-
-//#define RECORD_GAP_MICROS 12000 // Default is 8000. Activate it for some LG air conditioner protocols
-//#define DEBUG // Activate this for lots of lovely debug output from the decoders.
-
 #include <IRremote.hpp>
 
+#define IR_RECEIVE_PIN 11  // Pin para el receptor IR
+#define IR_SEND_PIN 3      // Pin para el emisor IR
 
-#define IR_LED_PIN 3
-const int ledPin = 13; // Define el pin al que está conectado el LED IR
-IRsend irsend(IR_LED_PIN);
-
-// Códigos IR genéricos comunes
-//Spectra
-unsigned long powerCode = 0xBB44BF40; // Código IR para encender/apagar
-unsigned long volumeUpCode = 0x2FD58A7; // Código IR para subir volumen
-unsigned long volumeDownCode = 0x2FD7887; // Código IR para bajar volumen
-unsigned long channelUpCode = 0x2FDD827; // Código IR para subir canal
-unsigned long channelDownCode = 0x2FDF807; // Código IR para bajar canal
-unsigned long muteCode = 0x2FD08F7; // Código IR para silenciar
-
-unsigned long sourceCode = 0x2FD28D7; // Código IR para home
-unsigned long upCode =0x2FD9867 ; // Código IR para arriba
-unsigned long downCode = 0x2FDB847  ; // Código IR para abajo
-unsigned long leftCode = 0x2FD629D  ; // Código IR para izquierda
-unsigned long rightCode = 0x2FDE21D  ; // Código IR para derecha
-unsigned long okCode = 0x2FD50AF  ; // Código IR para ok
-unsigned long backCode = 0x2FDFF00 ; // Código IR para regresar
-
-unsigned long playPauseCode = 0x2FDEA15 ; // Código IR para play/pause
-
-unsigned long exitCode = 0x2FD22DD  ;
-
-unsigned long menuCode = 0x2FDDA25    ; // Código IR para Netflix
-unsigned long disneyCode = 0x3A00 ; // Código IR para Disney+
-
-
-
-
-//+=============================================================================
-// Configure the Arduino
-//
 void setup() {
-  // Inicializar el puerto serie
-//  Serial1.begin(9600);
-  pinMode(ledPin, OUTPUT); // Configura el pin como salida
+    Serial.begin(115200); // Inicializa la comunicación serie
     pinMode(LED_BUILTIN, OUTPUT);
-
-    Serial.begin(115200);   // Status message will be sent to PC at 9600 baud
-#if defined(__AVR_ATmega32U4__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/|| defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
-    delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
-#endif
-    // Just to know which program is running on my Arduino
-    //Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
-
-    // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
-    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-
-    Serial.print(F("Ready to receive IR signals of protocols: "));
-   // printActiveIRProtocols(&Serial);
-   // Serial.println(F("at pin " STR(IR_RECEIVE_PIN)));
-
-    // infos for receive
-   // Serial.print(RECORD_GAP_MICROS);
-   // Serial.println(F(" us is the (minimum) gap, after which the start of a new IR packet is assumed"));
-   // Serial.print(MARK_EXCESS_MICROS);
-   // Serial.println();
-   // Serial.println(F("Because of the verbose output (>200 ms at 115200 baud), repeats are not dumped correctly!"));
-   // Serial.println();
-   // Serial.println(F("If you receive protocol NEC, Samsung or LG, run also ReceiveDemo to check if your actual protocol is eventually NEC2 or SamsungLG, which is determined by the repeats"));
-   // Serial.println();
-
+    
+    Serial.println(F("Iniciando receptor IR..."));
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Inicializa el receptor IR
+    
+    Serial.println(F("Listo para recibir señales IR."));
 }
 
-//+=============================================================================
-// The repeating section of the code
 void loop() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-    switch (command) {
-      case 'p':
-        irsend.sendNEC(powerCode, 32);
-        break;
-      case 'u':
-        irsend.sendNEC(volumeUpCode, 32);
-        break;
-      case 'd':
-        irsend.sendNEC(volumeDownCode, 32);
-        break;
-      case 'm':
-        irsend.sendNEC(muteCode, 32);
-        break;
-      case 'h':
-        irsend.sendNEC(sourceCode, 32);
-        break;
-      case 'U':
-        irsend.sendNEC(upCode, 32);
-        break;
-      case 'D':
-        irsend.sendNEC(downCode, 32);
-        break;
-      case 'L':
-        irsend.sendNEC(leftCode, 32);
-        break;
-      case 'R':
-        irsend.sendNEC(rightCode, 32);
-        break;
-      case 'O':
-        irsend.sendNEC(okCode, 32);
-        break;
-      case 'b':
-        irsend.sendNEC(backCode, 32);
-        break;
-      case 'P':
-        irsend.sendNEC(playPauseCode, 32);
-        break;
-      case 'n':
-        irsend.sendNEC(menuCode, 32);
-        break;
-      case 'i':
-        irsend.sendNEC(disneyCode, 32);
-        break;
-      case 'e':
-        irsend.sendNEC(exitCode, 32);
-        break;
-      default:
-        break;
+    if (IrReceiver.decode()) {  // Captura una señal IR
+        // Imprime los resultados de la señal recibida
+        Serial.println();
+        Serial.println(F("Señal recibida:"));
+        IrReceiver.printIRResultShort(&Serial);
+
+        // Retransmite la señal recibida
+        if (IrReceiver.decodedIRData.protocol != UNKNOWN) {
+            Serial.println(F("Retransmitiendo señal..."));
+            IrSender.sendIRResultRaw(&IrReceiver.decodedIRData.rawData); // Enviar la señal IR recibida
+            
+            // Imprime la señal que se retransmitió
+            Serial.println(F("Señal retransmitida:"));
+            IrReceiver.printIRResultShort(&Serial);
+        } else {
+            Serial.println(F("Protocolo desconocido, no se retransmitirá."));
+        }
+
+        IrReceiver.resume(); // Prepara para la siguiente señal IR
     }
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-  }
-
-  if (IrReceiver.decode()) {
-    Serial.println();
-    IrReceiver.printIRResultShort(&Serial);
-    Serial.println();
-    IrReceiver.resume();
-  }
-
 }
